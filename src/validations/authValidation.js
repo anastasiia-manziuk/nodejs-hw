bf1265777a7827856a66d597ea8282c8af9a0bcc
ANASTASIIA <nastamanzuk62@gmail.com>
2026-02-01 20:42:33 +0100

    update authValid

--- src/validations/authValidation.js
+++ src/validations/authValidation.js
@@ -10,6 +10,6 @@ export const registerUserSchema = {
 export const loginUserSchema = {
   [Segments.BODY]: Joi.object({
     email: Joi.string().email().required(),
-    password: Joi.string().min().required(),
+    password: Joi.string().required(),
   }),
 };
